619734f3e642eb426fea5166b5e49f9abd3997ac
akaori <akiyamaanaluiza@gmail.com>
2023-09-25 19:55:13 -0300

    add backend folder with generated springboot app

---
+++ backend/src/test/java/com/challenge/customerprospecting/CustomerprospectingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.customerprospecting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerprospectingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
