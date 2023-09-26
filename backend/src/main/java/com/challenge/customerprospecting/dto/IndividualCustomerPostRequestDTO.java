be70063f80d36965830bc27849bb44799b0acc88
akaori <akiyamaanaluiza@gmail.com>
2023-09-26 06:56:26 -0300

    import from jakarta and add new validations

--- backend/src/main/java/com/challenge/customerprospecting/dto/IndividualCustomerPostRequestDTO.java
+++ backend/src/main/java/com/challenge/customerprospecting/dto/IndividualCustomerPostRequestDTO.java
@@ -1,26 +1,34 @@
 package com.challenge.customerprospecting.dto;
 
 
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.*;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class IndividualCustomerPostRequestDTO {
 
     @NotBlank(message = "Nome inválido: nome vazio")
     @NotNull(message = "Nome inválido: nome nulo")
-    @Size(max = 50)
+    @Max(value = 50, message = "Nome inválido: Excede 50 caracteres")
     private String name;
 
     @NotBlank(message = "MCC inválido: MCC vazio")
     @NotNull(message = "MCC inválido: MCC nulo")
-    @Size(max = 4)
+    @Max(value = 4, message = "MCC inválido: Máximo de 4 caracteres")
+    @Min(value = 4, message = "MCC inválido: Mínimo de 4 caracteres")
     private String mcc;
 
     @NotBlank(message = "CPF inválido: CPF vazio")
     @NotNull(message = "CPF inválido: CPF nulo")
-    @Size(min = 11, max = 11)
+    @Max(value = 11, message = "CPF inválido: Máximo de 11 caracteres")
+    @Min(value = 11, message = "CPF inválido: Mínimo de 11 caracteres")
     private String cpf;
 
     @NotBlank(message = "Email inválido: Email vazio")
