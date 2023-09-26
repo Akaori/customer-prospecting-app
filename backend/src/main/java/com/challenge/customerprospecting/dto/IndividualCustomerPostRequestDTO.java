74aafcc443fb6977cfddf98c6ae575de2e9224b2
akaori <akiyamaanaluiza@gmail.com>
2023-09-25 23:06:32 -0300

    put validations at dto class

--- backend/src/main/java/com/challenge/customerprospecting/dto/IndividualCustomerPostRequestDTO.java
+++ backend/src/main/java/com/challenge/customerprospecting/dto/IndividualCustomerPostRequestDTO.java
@@ -3,10 +3,29 @@ package com.challenge.customerprospecting.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.*;
+
 @Data
 public class IndividualCustomerPostRequestDTO {
+
+    @NotBlank(message = "Nome inválido: nome vazio")
+    @NotNull(message = "Nome inválido: nome nulo")
+    @Size(max = 50)
     private String name;
+
+    @NotBlank(message = "MCC inválido: MCC vazio")
+    @NotNull(message = "MCC inválido: MCC nulo")
+    @Size(max = 4)
     private String mcc;
+
+    @NotBlank(message = "CPF inválido: CPF vazio")
+    @NotNull(message = "CPF inválido: CPF nulo")
+    @Size(min = 11, max = 11)
     private String cpf;
+
+    @NotBlank(message = "Email inválido: Email vazio")
+    @NotNull(message = "Email inválido: Email nulo")
+    @Email(message = "Email precisa ser válido")
+    @Pattern(regexp = "^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})$", message = "Email precisa ser válido")
     private String email;
 }
