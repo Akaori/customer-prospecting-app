7e605862642de979f7443ca06480a3729e3430fd
akaori <akiyamaanaluiza@gmail.com>
2023-09-26 06:57:40 -0300

    put validation annotations

--- backend/src/main/java/com/challenge/customerprospecting/controller/IndividualCustomerController.java
+++ backend/src/main/java/com/challenge/customerprospecting/controller/IndividualCustomerController.java
@@ -4,14 +4,18 @@ package com.challenge.customerprospecting.controller;
 import com.challenge.customerprospecting.dto.IndividualCustomerPostRequestDTO;
 import com.challenge.customerprospecting.entity.IndividualCustomer;
 import com.challenge.customerprospecting.service.IndividualCustomerService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
+@Validated
 @RequiredArgsConstructor
 @RequestMapping(value = "/api/v1/individual-customers", produces = "application/json")
 public class IndividualCustomerController {
@@ -19,9 +23,8 @@ public class IndividualCustomerController {
     private final IndividualCustomerService individualCustomerService;
 
     @PostMapping
-    public ResponseEntity<IndividualCustomer> save(@RequestBody IndividualCustomerPostRequestDTO individualCustomer) {
+    public ResponseEntity<IndividualCustomer> save(@RequestBody @Valid IndividualCustomerPostRequestDTO individualCustomer) {
         IndividualCustomer createdIndividualCustomer = individualCustomerService.save(individualCustomer);
         return ResponseEntity.ok().body(createdIndividualCustomer);
     }
-
 }
