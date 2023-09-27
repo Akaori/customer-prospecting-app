bbfabaa3efc774abedb5690710fe68dcee763fe6
akaori <akiyamaanaluiza@gmail.com>
2023-09-26 21:59:18 -0300

    add put endpoints

--- backend/src/main/java/com/challenge/customerprospecting/controller/LegalEntityCustomerController.java
+++ backend/src/main/java/com/challenge/customerprospecting/controller/LegalEntityCustomerController.java
@@ -1,6 +1,9 @@
 package com.challenge.customerprospecting.controller;
 
+import com.challenge.customerprospecting.dto.IndividualCustomerPutRequestDTO;
 import com.challenge.customerprospecting.dto.LegalEntityCustomerPostRequestDTO;
+import com.challenge.customerprospecting.dto.LegalEntityCustomerPutRequestDTO;
+import com.challenge.customerprospecting.entity.IndividualCustomer;
 import com.challenge.customerprospecting.entity.LegalEntityCustomer;
 import com.challenge.customerprospecting.service.LegalEntitycustomerService;
 import jakarta.validation.Valid;
@@ -8,10 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Validated
@@ -23,5 +23,15 @@ public class LegalEntityCustomerController {
     @PostMapping
     public ResponseEntity<LegalEntityCustomer> save(@RequestBody @Valid LegalEntityCustomerPostRequestDTO legalEntityCustomer) {
         return new ResponseEntity<LegalEntityCustomer>(legalEntitycustomerService.save(legalEntityCustomer), HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<LegalEntityCustomer> findById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(legalEntitycustomerService.findById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<LegalEntityCustomer> update(@PathVariable Long id, @RequestBody LegalEntityCustomerPutRequestDTO legalEntityCustomerPutRequestDTO) {
+        return ResponseEntity.ok(legalEntitycustomerService.update(legalEntityCustomerPutRequestDTO, id));
     }
 }
