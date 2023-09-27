a97bfc1eb4e9bb305e5220b5028c82c7782b9a56
akaori <akiyamaanaluiza@gmail.com>
2023-09-27 20:13:32 -0300

    setup routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,17 @@
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Home from "./scenes/home";
+// import AddLegalEntityCustomerForm from "./scenes/addlegalentitycustomerform";
+// import AddIndividualCustomerForm from "./scenes/addindividualcustomerform";
+// import EditLegalEntityCustomerForm from "./scenes/editlegalentitycustomerform";
+// import EditIndividualCustomerForm from "./scenes/editindividualcustomerform";
+// import LegalEntityCustomerList from "./scenes/legalentitycustomerlist";
+// import IndividualCustomerList from "./scenes/individualcustomerlist";
+// import ProspectLegalEntityCustomer from "./scenes/prospectlegalentitycustomer";
+// import ProspectIndividualCustomer from "./scenes/prospectindividualcustomer";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -9,7 +21,46 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <main className="content">
-          <div className="app"></div>
+          <Topbar />
+
+          <Routes>
+            <Route
+              path="/"
+              element={<Home />}
+            />
+            {/* <Route
+              path="/legal-entity-customer/add-customer-form"
+              element={<AddLegalEntityCustomerForm />}
+            /> */}
+            {/* <Route
+              path="/legal-entity-customer/update-customer-form"
+              element={<EditLegalEntityCustomerForm />}
+            /> */}
+            {/* <Route
+              path="/legal-entity-customer/list"
+              element={<LegalEntityCustomerList />}
+            /> */}
+            {/* <Route
+              path="/legal-entity-customer/prospect"
+              element={<ProspectLegalEntityCustomer />}
+            /> */}
+            {/* <Route
+              path="/individual-customer/add-customer-form"
+              element={<AddIndividualCustomerForm />}
+            /> */}
+            {/* <Route
+              path="/individual-customer/update-customer-form"
+              element={<EditIndividualCustomerForm />}
+            /> */}
+            {/* <Route
+              path="/individual-customer/list"
+              element={<IndividualCustomerList />}
+            /> */}
+            {/* <Route
+              path="/individual-customer/prospect"
+              element={<ProspectIndividualCustomer />}
+            /> */}
+          </Routes>
         </main>
       </ThemeProvider>
     </ColorModeContext.Provider>
