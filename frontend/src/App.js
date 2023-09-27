938afa0ff994804d01953313ad55ea43205a2306
akaori <akiyamaanaluiza@gmail.com>
2023-09-27 19:48:25 -0300

    configure theme in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,19 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
 function App() {
-  return <div className="app"></div>;
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <main className="content">
+          <div className="app"></div>
+        </main>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
 export default App;
